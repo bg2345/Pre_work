8aca683cfff8a249483468791e084d5826ef03f4
bg2345 <bgardynski@gmail.com>
2019-02-19 15:34:29 -0500

    Connected to Postgres database

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'random-word-random-word'
 
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABSE_URL') or \
-        'sqlite:///' + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:BRUINS4@localhost:5432/portfolio'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
